c74d052c15b53622328b9a3d3f758adc5efc1c4f
wzc <515819056@qq.com>
2019-01-20 21:05:44 +0800

    编辑和现实人格首页

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . \Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => '用户名已被占用，请重新填写',
+            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
+            'name.between' => '用户名必须介于 3 - 25 个字符之间。',
+            'name.required' => '用户名不能为空。',
+        ];
+    }
+}
