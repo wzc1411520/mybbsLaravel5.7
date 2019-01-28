82a8646bc50a2b0f512cf3eb27f0b9ce6d19f287
wzc <515819056@qq.com>
2019-01-28 17:48:07 +0800

    添加接口限制

--- routes/api.php
+++ routes/api.php
@@ -24,9 +24,35 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ],function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
+
+
+
+    $api->group([
+        'middleware' => 'api.throttle',
+    ], function($api) {
+        //登录/注册
+        $api->group([
+            'limit' => config('api.rate_limits.sign.limit'),
+            'expires' => config('api.rate_limits.sign.expires'),
+        ],function($api){
+
+
+            // 短信验证码
+            $api->post('verificationCodes', 'VerificationCodesController@store')
+                ->name('api.verificationCodes.store');
+            // 用户注册
+            $api->post('users', 'UsersController@store')
+                ->name('api.users.store');
+        });
+
+        //访问
+        $api->group([
+            'limit' => config('api.rate_limits.access.limit'),
+            'expires' => config('api.rate_limits.access.expires'),
+        ],function($api){
+
+        });
+    });
 
 
 
