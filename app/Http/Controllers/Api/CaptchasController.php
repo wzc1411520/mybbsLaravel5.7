ea2e29e9c30f7d448a09f4a5da801ab584d18883
wzc <515819056@qq.com>
2019-01-29 17:16:30 +0800

    添加获取图片验证码/手机验证码的接口

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\CaptchaRequest;
+use Gregwar\Captcha\CaptchaBuilder;
+use Illuminate\Http\Request;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchaRequest $request,CaptchaBuilder $captchaBuilder)
+    {
+        //缓存key
+        $key = 'captcha-'.str_random(15);
+        $phone = $request->phone;
+
+        //创建验证码
+        $captcha = $captchaBuilder->build();
+        //设置有效时间
+        $expiredAt = now()->addMinutes(2);
+
+        //加入缓存
+        \Cache::put($key,['phone'=>$phone,'code'=>$captcha->getPhrase()],$expiredAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+            'captcha_image_content' => $captcha->inline()
+        ];
+
+        return $this->response->array($result);
+
+    }
+}
--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -10,22 +10,41 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request,EasySms $easySms)
     {
-        $phone = $request->phone;
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
-        try {
-            $result = $easySms->send($phone, [
-//                'content'  => '您的验证码为: 6379',
-                'template' => 'SMS_142090066',
-                'data' => [
-                    'code' => $code
-                ],
-            ]);
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-            $message = $exception->getException('aliyun')->getMessage();
-            return $this->response->errorInternal($message ?? '短信发送异常');
+        //获取去图片验证码
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if(!$captchaData){
+            return  $this->response->error('图片验证码已失效');
         }
 
+        //判断验证码是否正确
+        if(!hash_equals($captchaData['code'],$request->captcha_code)){
+            //验证码错误  清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+
+        $phone = $captchaData['phone'];
+
+        if(!app()->environment('production')){
+            $code = '1234';
+        }else{
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+            try {
+                $result = $easySms->send($phone, [
+//                'content'  => '您的验证码为: 6379',
+                    'template' => 'SMS_142090066',
+                    'data' => [
+                        'code' => $code
+                    ],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                return $this->response->errorInternal($message ?? '短信发送异常');
+            }
+        }
         //缓存验证码
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
